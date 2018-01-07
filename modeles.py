190417d9d4973dd52afd15ee3bc153f2db164a66
SebastienBocquet <sbocquet@gmail.com>
2018-01-07 23:11:03 +0100

    Add files via upload

---
+++ modeles.py
@@ -0,0 +1,242 @@
+import numpy as np
+import sys
+
+
+class PID():
+
+    def __init__(self, kp, ki, kd, integralLimit, deltaFilter, heartbeatHz):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.integralLimit = integralLimit
+        self.deltaFilter = deltaFilter
+        self.dxFiltered = 0.
+        self.heartbeatHz = heartbeatHz
+        self.previousX = 0.
+        self.errorIntegral = 0.
+        self.proportionalTerm = 0.
+        self.integralTerm = 0.
+        self.derivativeTerm = 0.
+
+    def computeDerivative(self, x):
+        """Compute delta between current and previous input signal"""
+        self.previousX = x
+        return x - self.previousX
+
+    def exponentialFilter(self, dx):
+        """Low pass filtering of input signal derivative"""
+        dt = 1.0 / self.heartbeatHz
+        return (dx * 1.0 / self.deltaFilter + self.dxFiltered * (self.heartbeatHz - 1.0 / self.deltaFilter)) * dt
+
+    def compute(self, x, target):
+        """Compute PID output as the sum of the proportional, integral and derivative of (input x - target)"""
+
+        dx = self.computeDerivative(x)
+        self.dxFiltered = self.exponentialFilter(dx)
+
+        error = x - target
+        self.errorIntegral += error
+        self.errorIntegral = min(self.errorIntegral, self.integralLimit)
+        self.errorIntegral = max(self.errorIntegral, -self.integralLimit)
+        output = -error * self.kp
+        output -= self.errorIntegral * self.ki
+        output -= self.dxFiltered * self.kd
+
+        self.proportionalTerm = error * self.kp
+        self.integralTerm = self.errorIntegral * self.ki
+        self.derivativeTerm = self.dxFiltered * self.kd
+
+        return output
+
+
+
+
+
+class Battery():
+
+    def __init__(self, v0, capa, maxCurrent):
+        self.v0 = v0
+        self.capa = capa
+        self.maxCurrent = maxCurrent
+
+    def compute_V(self):
+        """Compute battery voltage"""
+        return self.v0
+
+
+class Engine():
+
+    def __init__(self, ki, kv, radius, rotorMass, rotorInternalRadius, rotorExternalRadius):
+        self.ki = ki
+        self.kv = kv
+        self.radius = radius
+        self.rotorMass = rotorMass
+        self.rotorInternalRadius = rotorInternalRadius
+        self.rotorExternalRadius = rotorExternalRadius
+        self.J = self.rotorMass * \
+            (0.5 * self.rotorExternalRadius**2 +
+             0.5 * self.rotorInternalRadius**2)
+        self.omega_eq = 0.
+
+    def compute_equilibrium_omega(self, propeller, Vs):
+        """Steady state engine rotation velocity"""
+        roots = np.roots(
+            [-propeller.kw, -self.ki / (self.radius * self.kv), self.ki * Vs / self.radius])
+        roots_positive = roots[np.where(roots >= 0)]
+        if len(roots_positive) != 1:
+            print('could not find a single positive solution for equilibrium omega')
+            print('roots are', roots)
+            sys.exit(1)
+        else:
+            self.omega_eq = roots_positive[0]
+            print('equilibrium omega at Vs max=',
+                  self.omega_eq * 60. / (2 * np.pi), 'rpm')
+
+    def getTorque(self, omega, Vs):
+        """Engine torque in engine axis frame"""
+        return (self.ki * Vs / self.radius) - (self.ki / (self.radius * self.kv)) * omega
+
+    def getJ(self):
+        """Inertial torque in engine axis frame"""
+        return self.J
+
+
+class ESC():
+    """Model for an electric engine controller"""
+
+    def __init__(self, frequency, maxThrottle):
+        self.frequency = frequency
+        self.maxThrottle = maxThrottle
+
+    def computeVs(self, throttle, accu):
+        """Compute output controller voltage (= engine voltage if no lost in the wires)"""
+        return accu.compute_V() * throttle / self.maxThrottle
+
+
+class Propeller():
+    """Model for a small scale (RC plane) propeller"""
+
+    def __init__(self, diameter, meanChord, bladeMass, nbIte):
+        self.cl = 1.23
+        self.cd	 = 1.23
+        self.diameter = diameter
+        self.meanChord = meanChord
+        self.bladeMass = bladeMass
+        self.kw = np.nan
+        self.J = 2 * 2. / 3 * self.bladeMass * (0.5 * diameter)**2
+
+    def initKw(self, rho):
+        """Initialize propeller torque coefficient"""
+        self.kw = 2 * 0.25 * rho * self.cd	 * \
+            (0.5 * self.diameter)**4 * self.meanChord
+
+    def calibrateCl(self, thrust, omega):
+        """If you know propeller thrust at a given rotation velocity,
+        you can calibrate the cl coefficient instead of using the default value"""
+        self.cl = np.sqrt(
+            thrust / (0.33 * rho * self.cl * self.meanChord * (0.5 * self.diameter)**3))
+
+    def getThrust(self, rho, omega):
+        """Propeller thrust in propeller frame (z = propeller axis)"""
+        thrust = 0.33 * rho * self.cl * self.meanChord * \
+            omega**2 * (0.5 * self.diameter)**3
+        return np.array([0., 0., thrust])
+
+    def getTorque(self, omega):
+        """Propeller torque along propeller axis"""
+        return -self.kw * omega**2
+
+    def getJ(self):
+        """Propeller inertial torque in propeller axis frame"""
+        return self.J
+
+
+class Point_mass():
+    """Model for a point mass in rotation around (0,0,0)"""
+
+    def __init__(self, mass, xG, yG, zG):
+        self.mass = mass
+        self.xG = xG
+        self.yG = yG
+        self.zG = zG
+        self.force = np.zeros((3))
+
+    def computeJ(self, rotAxis):
+        """Point mass inertial torque"""
+        return self.mass * np.linalg.norm(np.cross(rotAxis, np.array([self.xG, self.yG, self.zG])))
+
+    def setForce(self, force):
+        """Update force (3 element array or tuple) acting on the Point mass"""
+        self.force = force
+
+    def computeTorque(self, rotAxis):
+        """Compute Point mass torque around rotAxis (3 element array or tuple) at (0,0,0)"""
+        torqueVector = np.cross(
+            np.array([self.xG, self.yG, self.zG]), self.force)
+        return np.dot(rotAxis, torqueVector)
+
+
+class ODEintegration():
+    """Time integration of a first or second order ODE"""
+
+    def __init__(self, nbIte, dt):
+        self.nbIte = nbIte
+        self.dt = dt
+        self.f = np.zeros((nbIte))
+        self.fDot = np.zeros((nbIte))
+        self.fDotDot = np.zeros((nbIte))
+
+    def initF(self, f0):
+        """Initial solution for ODE solution f"""
+        self.f[:] = f0
+
+    def initFDot(self, fDot0):
+        """Initial solution for the derivative of ODE solution f.
+        Only necessary for second order ODE"""
+        self.fDot[:] = fDot0
+
+    def advanceInTimeFirstOrder(self, ite, fDot):
+        """Integration of first order ODE"""
+        self.fDot[ite] = fDot
+        self.f[ite + 1] = self.fDot[ite] * self.dt + self.f[ite]
+
+    def advanceInTimeSecondOrder(self, ite, fDotDot):
+        """Integration of second order ODE"""
+        self.fDotDot[ite] = fDotDot
+        self.f[ite + 1] = self.fDotDot[ite] * \
+            self.dt**2 + 2 * self.f[ite] - self.f[ite - 1]
+        self.fDot[ite + 1] = (self.f[ite + 1] - self.f[ite]) / self.dt
+
+    def getTimeAxis(self):
+        return np.arange(0., self.nbIte) * self.dt
+
+    def getF(self):
+        return self.f
+
+    def getFDot(self):
+        return self.fDot
+
+
+class Meca_model():
+    """Mecanical model consisting of N point masses in rotation around (0,0,0)"""
+
+    def __init__(self):
+        self.model = []
+
+    def add(self, pointMass):
+        """add a point mass to the model"""
+        self.model.append(pointMass)
+
+    def computeTotalJ(self, rot_axis):
+        """Compute inertial torque of the model"""
+        J = 0.
+        for m in self.model:
+            J += m.computeJ(rot_axis)
+        return J
+
+    def computeTotalTorque(self, rotAxis):
+        """Compute torque of the model around rotAxis at (0,0,0)"""
+        torque = 0.
+        for m in self.model:
+            torque += m.computeTorque(rotAxis)
+        return torque
---
+++ single_arm.py
@@ -0,0 +1,239 @@
+import sys
+print(sys.path)
+sys.path.append(
+    "C:\\Users\\Seb\\Documents\\Drone\\ConvertibleUAV\\Tools\\flight_analyzer")
+from modeles import *
+import matplotlib.pyplot as plt
+import toolbox as tool
+import time
+
+
+def run_dynamic_model():
+
+    # air
+    rho = 1.2
+
+    # PID control loop frequency in Hz
+    heartbeatHz = 160  
+
+    # Arm tilt PID control gains
+    tiltKi = 0.25
+    tiltKp = 0.5
+    tiltErrorIntegralLimit = 2000
+
+    # Arm tilt rate PID control gains
+    tiltRateKp = 0.22
+    tiltRateKd = 0.5
+    tiltRateErrorIntegralLimit = 2000
+    tiltRateFilterWidth = 1. / 160
+
+    # throttle order range
+    throttleRange = 2000
+
+    # frame dimensions
+    armLength = 0.5
+
+    # propulsion args
+    maxThrust = 11.  # in N
+
+    # constant throttle order. Throttle control will be throttleOffset + PID control
+    # throttleOffset should range between 0 and throttleRange
+    throttleOffset = 550.
+
+    # propeller
+    Cd = 1.23
+    bladeRadius = 0.5 * 10. * 0.0254
+    meanChord = 0.013
+    momentAeroCoef = 2 * 0.25 * rho * Cd * bladeRadius**4 * meanChord
+    bladeMass = 0.010
+    propellerMass = 2 * bladeMass
+
+    # battery
+    batteryMass = 0.18
+    V0 = 13.
+    capa = 2400.
+    maxCurrent = 20 * 2.4
+
+    # ESC
+    escFreq = 40000
+
+    # engine
+    engineMass = 0.07
+    rotorMass = 0.050
+    rotorInternalRadius = 0.015
+    rotorExternalRadius = 0.02
+    kv = 1100. * 2 * np.pi / 60
+    omegaMax = 1100 * V0 * 2 * np.pi / 60
+    ki = 5.3*momentAeroCoef * omegaMax**2 / 31
+    r = 0.5
+
+    # time advance
+    nbIte = 500000
+    deltaT = 1.e-5
+
+    # initial state
+    omega0 = 0.
+    theta0 = 0.
+
+    # target state
+    targetTheta = 0.
+
+    # plot args
+    fontsize = 12
+
+    # initialize objects
+
+    pidTheta = PID(tiltKp, tiltKi, 0., tiltErrorIntegralLimit, 1., heartbeatHz)
+    pidOmega = PID(tiltRateKp, 0., tiltRateKd, tiltRateErrorIntegralLimit, tiltRateFilterWidth, heartbeatHz)
+
+    esc = ESC(escFreq, throttleRange)
+    battery = Battery(V0, capa, maxCurrent)
+    engine = Engine(ki, kv, R, rotorMass,
+                    rotorInternalRadius, rotorExternalRadius)
+    propeller = Propeller(2 * bladeRadius, meanChord, bladeMass, nbIte)
+    propeller.initKw(rho)
+
+    # construct mecanical model
+    # an engine with propeller at the end of an arm aligned with y and rotating around x
+    #      z
+    #     ^
+    #     |   --|-- propeller
+    #     o-----M engine1
+    #     x     y
+
+    # Engine + propeller is modelled as a point mass
+    pmM1 = Point_mass(engineMass + propellerMass, 0., 0.5 * armLength, 0.)
+
+    quadri = Meca_model()
+    quadri.add(pmM1)
+
+    # rotation axis
+    x = (1, 0, 0)
+
+    # mecanical model inertia and torque
+    jx = quadri.computeTotalJ(x)
+    mx = quadri.computeTotalTorque(x)
+    print ("inertial x momentum", jx)
+    print ("total x torque", mx)
+
+    # time solver for engine rotation angle
+    ode1 = ODEintegration(nbIte, deltaT)
+    ode1.initF(0.)
+    ode1.initFDot(0.)
+
+    # time solver for arm ration angle
+    odex = ODEintegration(nbIte, deltaT)
+    odex.initF(theta0)
+    odex.initFDot(omega0)
+
+    # record quantities for post-processing
+    outputs = {}
+    outputs['vs1'] = np.empty((nbIte))
+    outputs['thrust1'] = np.empty((nbIte))
+    outputs['pidPTheta'] = np.empty((nbIte))
+    outputs['pidITheta'] = np.empty((nbIte))
+    outputs['pidDTheta'] = np.empty((nbIte))
+    outputs['pidPOmega'] = np.empty((nbIte))
+    outputs['pidIOmega'] = np.empty((nbIte))
+    outputs['pidDOmega'] = np.empty((nbIte))
+    outputs['thetaControl'] = np.empty((nbIte))
+
+
+    time0 = time.time()
+
+    # time loop
+
+    jProp = propeller.getJ() + engine.getJ()
+    vOffset = throttleOffset * V0 / throttleRange
+    vs1 = vOffset
+    thetaControl = 0.
+
+    # Time loop
+    for ite in range(1, nbIte - 1):
+
+        # Dynamic response
+
+        engineTorque1 = propeller.getTorque(
+            ode1.fDot[ite]) + engine.getTorque(ode1.fDot[ite], vs1)
+        ode1.advanceInTimeSecondOrder(ite, engineTorque1 / jProp)
+
+        # gravty force acting on the point mass
+        gravityForce1 = np.array([0.,
+            -pmM1.mass * 9.81 * np.sin(odex.f[ite]),
+            -pmM1.mass * 9.81 * np.cos(odex.f[ite])])
+
+        # force acting on the force mass is propeller thrust + gravity force
+        pmM1.setForce(propeller.getThrust(rho, ode1.fDot[ite]) + gravityForce1)
+        totalTorque = quadri.computeTotalTorque(x)
+        mx = totalTorque
+        odex.advanceInTimeSecondOrder(ite, mx / jx)
+
+        # PID retroaction on arm angle. Retroaction frequency is heartbeatHz
+
+        if (ite % (int)((1. / heartbeatHz) / deltaT) == 0):
+
+            print ('retroaction time', ite * deltaT)
+
+            # PID control on arm rotation velocity
+            targetOmega = pidTheta.compute(odex.f[ite], targetTheta)
+
+            # PID control on arm angle
+            targetOmegaDot = pidOmega.compute(odex.fDot[ite], targetOmega)
+            thetaControl = targetOmegaDot
+
+            # engine controller voltage
+            vs1 = esc.computeVs(throttleOffset + thetaControl, battery)
+
+        # update outputs
+        outputs['thrust1'][ite] = np.linalg.norm(propeller.getThrust(rho, ode1.fDot[ite]))
+        outputs['vs1'][ite] = vs1
+        outputs['pidPTheta'][ite] = pidTheta.proportionalTerm
+        outputs['pidITheta'][ite] = pidTheta.integralTerm
+        outputs['pidDTheta'][ite] = pidTheta.derivativeTerm
+        outputs['pidPOmega'][ite] = pidTheta.proportionalTerm
+        outputs['pidIOmega'][ite] = pidTheta.integralTerm
+        outputs['pidDOmega'][ite] = pidTheta.derivativeTerm
+        outputs['thetaControl'][ite] = thetaControl
+
+    print(time.time() - time0)
+
+
+    # Output plotting
+
+    fig, (ax1) = plt.subplots(1, 1, sharex=True)
+
+    xlabel = ''
+    ylabel = ''
+
+    fig.subplots_adjust(hspace=0.5)
+
+    ax1.set_title('', fontweight='bold', fontsize=fontsize)
+
+    ax1.plot(ode1.getTimeAxis(),  outputs['vs1'] / V0,
+             'r-', label='Vs')
+    ax1.plot(ode1.getTimeAxis(), outputs['thrust1'] / maxThrust,
+             'b-', label='propeller thrust in N')
+    ax1.plot(ode1.getTimeAxis(), outputs['pidPTheta'],
+             'g-', label='pid theta p')
+    ax1.plot(ode1.getTimeAxis(), outputs['pidITheta'],
+             'g-', label='pid theta i')
+    ax1.plot(ode1.getTimeAxis(), outputs['pidDTheta'],
+             'g--', label='pid theta d')
+    ax1.plot(ode1.getTimeAxis(), outputs['pidPOmega'],
+             'g:', label='pid omega p')
+    ax1.plot(ode1.getTimeAxis(), outputs['pidIOmega'],
+             'y-', label='pid omega i')
+    ax1.plot(ode1.getTimeAxis(), outputs['pidDOmega'],
+             'y--', label='pid omega d')
+    ax1.plot(ode1.getTimeAxis(), outputs['thetaControl'],
+             'y:', label='theta control')
+    ax1.plot(odex.getTimeAxis(), odex.getFDot(), 'k--', label='rotation velocity around x (rad/s)')
+    ax1.plot(odex.getTimeAxis(), odex.getF() * 180 / np.pi, 'k-', label='angle around x (deg)')
+
+    tool.finalize_plot(fig, ax1, None, None, None, None, xlabel, ylabel, fontsize, export_dir='', output_file='',
+                       show_legend=True, legend_type='outer_left', logscale_x=False, logscale_y=False, 
+                       show=True, tick_fontsize=None)
+
+if __name__ == '__main__':
+
+    run_dynamic_model()
